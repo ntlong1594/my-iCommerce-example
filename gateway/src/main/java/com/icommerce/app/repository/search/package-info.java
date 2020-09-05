72a3f4e5b63e831d2665c8229bce4e36c5947170
Long Thanh Nguyen <ntlong1594@gmail.com>
2020-09-05 13:05:23 +0700

    Initial version of gateway generated by JHipster-6.10.1

---
+++ gateway/src/main/java/com/icommerce/app/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package com.icommerce.app.repository.search;
---
+++ gateway/src/main/java/com/icommerce/app/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package com.icommerce.app.web.websocket.dto;
